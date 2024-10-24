a788b43ff694a6515787c2c18ae54de85a9e681f
Lars Krämer <lars.kraemer@live.de>
2024-10-24 11:24:16 +0200

    NDT-1: add menu changes

--- source/NugetDepTree/Platforms/MacCatalyst/AppDelegate.cs
+++ source/NugetDepTree/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,4 +1,5 @@
 ﻿using Foundation;
+using UIKit;
 
 namespace NugetDepTree;
 
@@ -6,4 +7,20 @@ namespace NugetDepTree;
 public class AppDelegate : MauiUIApplicationDelegate
 {
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	public override void BuildMenu(IUIMenuBuilder builder)
+	{
+		base.BuildMenu(builder);
+    
+		builder.RemoveMenu(UIMenuIdentifier.Edit.GetConstant());
+		builder.RemoveMenu(UIMenuIdentifier.Font.GetConstant());
+		builder.RemoveMenu(UIMenuIdentifier.Format.GetConstant());
+		builder.RemoveMenu(UIMenuIdentifier.Services.GetConstant());
+		builder.RemoveMenu(UIMenuIdentifier.Hide.GetConstant());
+    
+		builder.RemoveMenu(UIMenuIdentifier.Close.GetConstant());
+		builder.RemoveMenu(UIMenuIdentifier.Document.GetConstant());
+
+		builder.System.SetNeedsRebuild();
+	}
 }
